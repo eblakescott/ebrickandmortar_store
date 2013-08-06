40b740727458ab68e96defb76ea16787584a1f59
Ed Scott <eblakescott@gmail.com>
2013-08-05 19:17:22 -0500

    created new rails app; added Spree ecommerce platform

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EbrickandmortarStore::Application.config.secret_token = 'e307a4dcf64efee492d3771954c862e92008332dd61cec98a8c5dce28733048f24a9647dba460c18fc4113fb85abbf209b1e33f06306e41fc11b07bb28d75f05'
